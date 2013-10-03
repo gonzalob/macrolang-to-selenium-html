b59cf6df4b9fd5548f6a48a8f25669b493916dca
Gonzalo Bermúdez <gonzalob@gonz0.com.ar>
2013-10-03 15:58:28 -0300

    Skeleton for the project

---
+++ src/main/java/dridco/maven/GenerateSeleniumTestsMojo.java
@@ -0,0 +1,16 @@
+package dridco.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @goal run
+ * @phase="generate-test-sources"
+ */
+public class GenerateSeleniumTestsMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+	}
+}
