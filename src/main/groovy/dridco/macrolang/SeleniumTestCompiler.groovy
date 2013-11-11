67bda420bf2f72c52174b438c26dcf95e8bfb7f2
Gonzalo Bermúdez <gonzalob@gonz0.com.ar>
2013-11-11 17:53:24 -0300

    Dynamically generate simple test cases

--- src/main/groovy/dridco/macrolang/SeleniumTestCompiler.groovy
+++ src/main/groovy/dridco/macrolang/SeleniumTestCompiler.groovy
@@ -1,23 +1,45 @@
 package dridco.macrolang
 
+import static org.apache.commons.lang.StringUtils.EMPTY
+
 class SeleniumTestCompiler {
 
-	def compile(source) {
-		"""<?xml version="1.0" encoding="UTF-8"?>
+    static compile(String source) {
+        XmlParser parser = new XmlParser()
+        def test = parser.parseText source
+        def commands = test.command
+        def encoding = test.'@encoding'
+        def base = test.'@base'
+        def title = test.'@title'
+        def rows = new StringBuilder()
+        commands.each {
+            rows.append parseCommand( it )
+        }
+        """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head profile="http://selenium-ide.openqa.org/profiles/test-case">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="selenium.base" href="http://localhost" />
-<title>no commands</title>
+<meta http-equiv="Content-Type" content="text/html; charset=${encoding}" />
+<link rel="selenium.base" href="${base}" />
+<title>${title}</title>
 </head>
 <body>
 <table cellpadding="1" cellspacing="1" border="1">
 <thead>
 <tr><td rowspan="1" colspan="3">no commands</td></tr>
 </thead><tbody>
+${ rows }
 </tbody></table>
 </body>
 </html>"""
 	}
+
+    static parseCommand( source ) {
+        def value = source.'@value' ?: EMPTY
+        """<tr>
+    <td>${source.'@name'}</td>
+    <td>${source.'@target'}</td>
+    <td>${value}</td>
+</tr>"""
+    }
 }
--- src/test/groovy/dridco/macrolang/SeleniumTestCompilerTest.groovy
+++ src/test/groovy/dridco/macrolang/SeleniumTestCompilerTest.groovy
@@ -1,16 +1,14 @@
 package dridco.macrolang
 
-import static org.junit.Assert.*
+import static dridco.macrolang.SeleniumTestCompiler.compile
 
 import org.junit.Test
 
 class SeleniumTestCompilerTest {
 
-	private tested = new SeleniumTestCompiler()
-
 	@Test
 	void "can parse an empty test"() {
-		assert tested.compile("<test title='no commands' encoding='UTF-8' base='http://localhost'></test>") == """<?xml version="1.0" encoding="UTF-8"?>
+		assert compile("<?xml version='1.0' encoding='UTF-8' standalone='yes'?><test title='no commands' encoding='UTF-8' base='http://localhost'></test>") == """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head profile="http://selenium-ide.openqa.org/profiles/test-case">
@@ -23,8 +21,34 @@ class SeleniumTestCompilerTest {
 <thead>
 <tr><td rowspan="1" colspan="3">no commands</td></tr>
 </thead><tbody>
+
 </tbody></table>
 </body>
 </html>"""
 	}
+
+    @Test
+    void "can parse simple commands with attributes"() {
+        assert compile("<?xml version='1.0' encoding='UTF-8' standalone='yes'?><test title='one command' encoding='UTF-8' base='http://localhost'><command name='waitForElementPresent' target='id=foo' /></test>") == """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="http://localhost" />
+<title>one command</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">no commands</td></tr>
+</thead><tbody>
+<tr>
+    <td>waitForElementPresent</td>
+    <td>id=foo</td>
+    <td></td>
+</tr>
+</tbody></table>
+</body>
+</html>"""
+    }
 }
