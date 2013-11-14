95da503b3f0097c54ec372c74370b6c897ecf84a
Gonzalo Bermúdez <gonzalob@gonz0.com.ar>
2013-11-14 17:15:12 -0300

    Failing test, to check the invalid condition

---
+++ src/test/groovy/dridco/macrolang/NewAttributeInNestedMacroTest.groovy
@@ -0,0 +1,37 @@
+package dridco.macrolang
+
+class NewAttributeInNestedMacroTest extends AbstractCommandTest {
+
+    @Override
+    protected macros() {
+        ["""
+<macro name="outer">
+    <inner parameter="hello world" />
+</macro>
+""",
+                """
+<macro name="inner">
+    <command name="echo" target="\$parameter" />
+</macro>
+"""]
+
+    }
+
+    @Override
+    protected expectedCommands() {
+        """
+<tr>
+    <td>echo</td>
+    <td>hello world</td>
+    <td></td>
+</tr>
+"""
+    }
+
+    @Override
+    protected definedCommands() {
+        """
+<outer />
+"""
+    }
+}
